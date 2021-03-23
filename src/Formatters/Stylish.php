b6d52faa81f81a9ea04b9dd1d23c37b629e463a2
bl1ndy <ayoblindstone@gmail.com>
2021-03-23 15:20:37 +0300

    add stylish formatter in src/Formatters/Stylish.php

--- src/Formatters/Stylish.php
+++ src/Formatters/Stylish.php
@@ -2,11 +2,72 @@
 
 namespace Differ\Formatters\Stylish;
 
-use Exception;
-
 use function Functional\flatten;
 
-function stylish(array $diff): string
+function getIndent($depth)
 {
-    return 'function not done yet';
+    return str_repeat('    ', $depth);
+}
+
+function formatValue($value, $depth)
+{
+    $formatComplexValue = function ($value, $depth) {
+        $indent = getIndent($depth);
+        $complexValue = array_map(
+            function ($key, $value) use ($depth, $indent) {
+                return "{$indent}    {$key}: " . formatValue($value, $depth);
+            },
+            array_keys($value),
+            $value
+        );
+
+        return implode("\n", ["{", ...$complexValue, "{$indent}}"]);
+    };
+
+    $typeFormats = [
+        'string' => fn($value) => $value,
+        'integer' => fn($value) => (string) $value,
+        'object' => fn($value) => $formatComplexValue(get_object_vars($value), $depth + 1),
+        'array' => fn($value) => $formatComplexValue($value, $depth + 1),
+        'boolean' => fn($value) => $value ? "true" : "false",
+        'NULL' => fn($value) => 'null'
+    ];
+
+    $type = gettype($value);
+
+    return $typeFormats[$type]($value);
+}
+
+function stylish(array $diff, int $depth = 0): string
+{
+    $indent = getIndent($depth);
+    $lines = array_map(
+        function ($node) use ($indent, $depth) {
+            switch ($node['type']) {
+                case 'added':
+                    $formattedValue = formatValue($node['newValue'], $depth);
+                    return "{$indent}  + {$node['key']}: {$formattedValue}";
+                case 'removed':
+                    $formattedValue = formatValue($node['oldValue'], $depth);
+                    return "{$indent}  - {$node['key']}: {$formattedValue}";
+                case 'updated':
+                    $oldValue = formatValue($node['oldValue'], $depth);
+                    $newValue = formatValue($node['newValue'], $depth);
+                    return "{$indent}  - {$node['key']}: {$oldValue}"
+                        . "\n" . "{$indent}  + {$node['key']}: {$newValue}";
+                case 'unchanged':
+                    $formattedValue = formatValue($node['newValue'], $depth);
+                    return "{$indent}    {$node['key']}: {$formattedValue}";
+                case 'complex':
+                    $nestedIndent = getIndent($depth + 1);
+                    return "{$nestedIndent}{$node['key']}: "
+                        . stylish($node['children'], $depth + 1);
+                default:
+                    throw new \Exception("Node type '{$node['type']}' is invalid");
+            }
+        },
+        $diff
+    );
+
+    return implode("\n", ["{", ...$lines, "{$indent}}"]);
 }
