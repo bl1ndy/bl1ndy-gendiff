dff986fc6f49d90ddb6ffd295b94f5078ba10abc
bl1ndy <ayoblindstone@gmail.com>
2021-03-23 15:42:56 +0300

    add plain formatter to src/Formatters/Plain.php

--- src/Formatters/Plain.php
+++ src/Formatters/Plain.php
@@ -4,7 +4,48 @@ namespace Differ\Formatters\Plain;
 
 use function Functional\flatten;
 
-function plain(array $diff): string
+function formatValue($value)
 {
-    return 'function not done yet';
+    $typeFormats = [
+        'string' => fn($value) => "'$value'",
+        'integer' => fn($value) => (string) $value,
+        'object' => fn($value) => '[complex value]',
+        'array' => fn($value) => '[complex value]',
+        'boolean' => fn($value) => $value ? 'true' : 'false',
+        'NULL' => fn($value) => 'null'
+    ];
+
+    $type = gettype($value);
+
+    return $typeFormats[$type]($value);
+}
+
+function plain(array $diff, array $path = []): string
+{
+    $lines = array_map(
+        function ($node) use ($path) {
+            $fullPropertyPath = implode('.', [...$path, $node['key']]);
+            switch ($node['type']) {
+                case 'added':
+                    $formattedValue = formatValue($node['newValue']);
+                    return "Property '$fullPropertyPath' was added with value: {$formattedValue}";
+                case 'removed':
+                    $formattedValue = formatValue($node['oldValue']);
+                    return "Property '$fullPropertyPath' was removed";
+                case 'updated':
+                    $oldValue = formatValue($node['oldValue']);
+                    $newValue = formatValue($node['newValue']);
+                    return "Property '$fullPropertyPath' was updated. From $oldValue to $newValue";
+                case 'unchanged':
+                    return [];
+                case 'complex':
+                    return plain($node['children'], [...$path, $node['key']]);
+                default:
+                    throw new \Exception("Node type '{$node['type']}' is invalid");
+            }
+        },
+        $diff
+    );
+
+    return implode("\n", flatten($lines));
 }
