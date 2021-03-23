752dfcd6fd26f225c3ff8f67338f845024fd45d5
bl1ndy <ayoblindstone@gmail.com>
2021-03-23 15:20:08 +0300

    edit getDiff() function in src/Differ.php

--- src/Differ.php
+++ src/Differ.php
@@ -7,19 +7,19 @@ use function Differ\Formatters\format;
 use function Functional\sort;
 
 /**
- * @param string $type
  * @param string $key
+ * @param string $type
  * @param mixed  $oldValue
  * @param mixed  $newValue
  * @param array  $children
  *
  * @return array
  */
-function getNode($type, $key, $oldValue, $newValue, $children = [])
+function makeNode($key, $type, $oldValue, $newValue, $children = null)
 {
     return [
-        'type' => $type,
         'key' => $key,
+        'type' => $type,
         'oldValue' => $oldValue,
         'newValue' => $newValue,
         'children' => $children
@@ -39,18 +39,18 @@ function getDiff(object $object1, object $object2): array
         array_map(
             function ($key) use ($object1, $object2): array {
                 if (!property_exists($object1, $key)) {
-                    return getNode('added', $key, null, $object2->{$key});
+                    return makeNode($key, 'added', null, $object2->$key);
                 }
                 if (!property_exists($object2, $key)) {
-                    return getNode('removed', $key, $object1->{$key}, null);
+                    return makeNode($key, 'removed', $object1->$key, null);
                 }
-                if ($object1->{$key} === $object2->{$key}) {
-                    return getNode('unchanged', $key, $object1->{$key}, $object2->{$key});
+                if ($object1->$key === $object2->$key) {
+                    return makeNode($key, 'unchanged', $object1->$key, $object2->$key);
                 }
-                if (is_object($object1->{$key}) && is_object($object2->{$key})) {
-                    return getNode('complex', $key, null, null, getDiff($object1->{$key}, $object2->{$key}));
+                if (is_object($object1->{$key}) && is_object($object2->$key)) {
+                    return makeNode($key, 'complex', null, null, getDiff($object1->$key, $object2->$key));
                 }
-                return getNode('changed', $key, $object1->{$key}, $object2->{$key});
+                return makeNode($key, 'updated', $object1->$key, $object2->$key);
             },
             $keys
         )
